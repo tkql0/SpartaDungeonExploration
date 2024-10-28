0f70f02e9ee08d20d58efa2277f574bcf13780d0
tkql0 <96943505+tkql0@users.noreply.github.com>
2024-10-29 03:11:11 +0900

    [Update]_PlayerController.cs [Chore]Package
    [Update]_PlayerController.cs
     - 움직임 구현
    
    [Chore]Package
     - 새로운 패키지 다운

--- Assets/4.Scripts/PlayerController.cs
+++ Assets/4.Scripts/PlayerController.cs
@@ -5,34 +5,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Movement")]
+    [Header("Move")]
     public float moveSpeed;
-    private Vector2 curMovementInput;
-    public float jumptForce;
+    public float jumpPower;
+    private Vector2 _inputVector;
     public LayerMask groundLayerMask;
 
-    [Header("Look")]
-    public Transform cameraContainer;
-    public float minXLook;
-    public float maxXLook;
-    private float camCurXRot;
-    public float lookSensitivity;
-
-    private Vector2 mouseDelta;
-
-    [HideInInspector]
-    public bool canLook = true;
-
-    private Rigidbody rigidbody;
+    private Rigidbody _rigidbody;
+    private Animator _animator;
 
     private void Awake()
     {
-        rigidbody = GetComponent<Rigidbody>();
-    }
-
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     private void FixedUpdate()
@@ -40,81 +25,26 @@ public class PlayerController : MonoBehaviour
         Move();
     }
 
-    private void LateUpdate()
-    {
-        if (canLook)
-        {
-            CameraLook();
-        }
-    }
-
-    public void OnLookInput(InputAction.CallbackContext context)
-    {
-        mouseDelta = context.ReadValue<Vector2>();
-    }
-
-    public void OnMoveInput(InputAction.CallbackContext context)
-    {
-        if (context.phase == InputActionPhase.Performed)
-        {
-            curMovementInput = context.ReadValue<Vector2>();
-        }
-        else if (context.phase == InputActionPhase.Canceled)
-        {
-            curMovementInput = Vector2.zero;
-        }
-    }
-
-    public void OnJumpInput(InputAction.CallbackContext context)
-    {
-        if (context.phase == InputActionPhase.Started && IsGrounded())
-        {
-            rigidbody.AddForce(Vector2.up * jumptForce, ForceMode.Impulse);
-        }
-    }
-
     private void Move()
     {
-        Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
-        dir *= moveSpeed;
-        dir.y = rigidbody.velocity.y;
-
-        rigidbody.velocity = dir;
+        Vector3 dir = new Vector3(_inputVector.x, 0, _inputVector.y).normalized
+             * moveSpeed * Time.deltaTime;
+        _rigidbody.MovePosition(_rigidbody.position + dir);
     }
 
-    void CameraLook()
-    {
-        camCurXRot += mouseDelta.y * lookSensitivity;
-        camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
-        cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
-
-        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
-    }
-
-    bool IsGrounded()
-    {
-        Ray[] rays = new Ray[4]
-        {
-            new Ray(transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
-            new Ray(transform.position + (-transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
-            new Ray(transform.position + (transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down),
-            new Ray(transform.position + (-transform.right * 0.2f) +(transform.up * 0.01f), Vector3.down)
-        };
-
-        for (int i = 0; i < rays.Length; i++)
-        {
-            if (Physics.Raycast(rays[i], 0.1f, groundLayerMask))
-            {
-                return true;
-            }
+    public void OnMove(InputAction.CallbackContext inContext)
+    { // InputSystem에서 값 받아오기
+        if (inContext.phase == InputActionPhase.Started)
+        { // 만약 키보드를 눌렸다면
+            _inputVector = inContext.ReadValue<Vector2>();
+            // Vector2 값 받아오기
+            _animator.SetBool("isMove", true);
         }
-
-        return false;
-    }
-
-    public void ToggleCursor(bool toggle)
-    {
-        Cursor.lockState = toggle ? CursorLockMode.None : CursorLockMode.Locked;
-        canLook = !toggle;
+        else if (inContext.phase == InputActionPhase.Canceled)
+        { // 키보드를 땠다면
+            _inputVector = Vector2.zero;
+            // _inputVector 값 초기화
+            _animator.SetBool("isMove", false);
+        }
     }
 }
